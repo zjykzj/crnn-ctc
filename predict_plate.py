9a50484b6fc0d00b0f28faaf9464f4b083460846
zjkjzj <wy163zhuj@163.com>
2024-07-21 17:17:17 +0800

    perf(predict_plate.py): udpate predict()

--- predict_plate.py
+++ predict_plate.py
@@ -18,10 +18,8 @@ from itertools import groupby
 
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
-
-from utils.model.crnn_gru import CRNN
-from utils.dataset.plate import PLATE_CHARS
 
 # cp assets/fonts/simhei.ttf /usr/share/fonts/truetype/noto/
 # rm -rf ~/.cache/matplotlib/*
@@ -42,19 +40,33 @@ def parse_opt():
 
 
 @torch.no_grad()
-def predict(image_path, pretrained, save_dir):
+def predict(image=None, image_path=None, pretrained=None, save_dir=None, is_show=False):
+    if image is None:
+        from utils.model.crnn_gru import CRNN
+        from utils.dataset.plate import PLATE_CHARS
+    else:
+        from .utils.model.crnn_gru import CRNN
+        from .utils.dataset.plate import PLATE_CHARS
+
     model = CRNN(in_channel=3, num_classes=len(PLATE_CHARS), cnn_output_height=9)
-    print(f"Loading CRNN pretrained: {pretrained}")
-    ckpt = torch.load(pretrained, map_location='cpu')
-    ckpt = {k.replace("module.", ""): v for k, v in ckpt.items()}
-    model.load_state_dict(ckpt, strict=True)
+    if pretrained is not None:
+        if isinstance(pretrained, list):
+            pretrained = pretrained[0]
+        print(f"Loading CRNN pretrained: {pretrained}")
+        ckpt = torch.load(pretrained, map_location='cpu')
+        ckpt = {k.replace("module.", ""): v for k, v in ckpt.items()}
+        model.load_state_dict(ckpt, strict=True)
     model.eval()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
 
     # Data
-    image = cv2.imread(image_path)
+    if image_path is not None:
+        assert os.path.isfile(image_path), image_path
+        image = cv2.imread(image_path)
+    else:
+        assert isinstance(image, np.ndarray)
     if image.shape[-1] == 4:
         image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
     img_w = 168
@@ -78,25 +90,35 @@ def predict(image_path, pretrained, save_dir):
     pred = pred.numpy()
 
     pred_plate = [PLATE_CHARS[i] for i in pred]
-    pred_plate = ''.join(pred_plate)
+    # pred_plate = ''.join(pred_plate)
+    pred_plate = ''.join(pred_plate[:2]) + "·" + ''.join(pred_plate[2:])
 
-    # Draw
-    plt.figure()
-    title = f"Pred: {pred_plate}"
-    print(title)
+    if is_show:
+        # Draw
+        plt.figure()
+        title = f"Pred: {pred_plate}"
+        print(title)
 
-    plt.title(title)
-    plt.imshow(image)
-    plt.axis('off')
+        plt.title(title)
+        plt.imshow(image)
+        plt.axis('off')
 
-    image_name = os.path.basename(image_path)
-    plt.savefig(os.path.join(save_dir, f"plate_{image_name}"))
+        if image_path is not None:
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+
+            image_name = os.path.basename(image_path)
+            res_path = os.path.join(save_dir, f"plate_{image_name}")
+            print(f'Save to {res_path}')
+            plt.savefig(res_path)
+
+    return pred_plate
 
 
 def main():
     args = parse_opt()
 
-    predict(args.image_path, args.pretrained, args.save_dir)
+    predict(image_path=args.image_path, pretrained=args.pretrained, save_dir=args.save_dir, is_show=True)
 
 
 if __name__ == '__main__':
