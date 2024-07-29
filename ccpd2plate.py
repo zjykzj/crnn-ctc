76cd3b8e9665136cfc3c3ba47e1462a347ef29ff
zjykzj <wy163zhuj@163.com>
2024-07-29 20:36:52 +0800

    perf(ccpd2plate.py): update

--- ccpd2plate.py
+++ ccpd2plate.py
@@ -7,11 +7,23 @@
 @Description:
 """
 
+import sys
+
 import cv2
 import glob
 import os.path
 
 from tqdm import tqdm
+
+# 获取当前脚本的绝对路径
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# 假设你的模块在上级目录中
+parent_dir = os.path.dirname(script_dir)
+
+# 添加模块所在的目录到 sys.path
+sys.path.insert(0, os.path.join(parent_dir))
+
+from utils.dataset.plate import DELIMITER
 
 provinces = ["皖", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "苏", "浙", "京", "闽", "赣", "鲁", "豫", "鄂",
              "湘", "粤", "桂", "琼", "川", "贵", "云", "藏", "陕", "甘", "青", "宁", "新", "警", "学", "O"]
@@ -47,6 +59,21 @@ def parse_ccpd(file_name):
     return box_xyxy, plate_name
 
 
+def save_to_dst(file_path, file_name, dst_data_root):
+    box_xyxy, plate_name = parse_ccpd(file_name)
+
+    src_img = cv2.imread(file_path)
+    x1, y1, x2, y2 = box_xyxy
+    plate_img = src_img[y1:y2, x1:x2]
+
+    dst_file_path = os.path.join(dst_data_root, plate_name + f'.jpg')
+    if os.path.exists(dst_file_path):
+        file_len = len(list(glob.glob(os.path.join(dst_data_root, f"{plate_name}*.jpg"))))
+        dst_file_path = os.path.join(dst_data_root, plate_name + f'{DELIMITER}{file_len}.jpg')
+    # assert not os.path.exists(dst_file_path), f"{file_name}\n{dst_file_path}"
+    cv2.imwrite(dst_file_path, plate_img)
+
+
 def process_ccpd2019(data_root, dst_root):
     for name in ['splits/train.txt', 'splits/val.txt', 'splits/test.txt']:
         txt_path = os.path.join(data_root, name)
@@ -54,10 +81,8 @@ def process_ccpd2019(data_root, dst_root):
         print('*' * 100)
         print(f"Getting {txt_path} data...")
 
-        if 'test' not in name:
-            dst_data_root = os.path.join(dst_root, "trainval")
-        else:
-            dst_data_root = os.path.join(dst_root, "test")
+        cls_name = os.path.basename(name).split('.')[0]
+        dst_data_root = os.path.join(dst_root, cls_name)
         if not os.path.exists(dst_data_root):
             os.makedirs(dst_data_root)
         print(f"Save to {dst_data_root}")
@@ -72,18 +97,7 @@ def process_ccpd2019(data_root, dst_root):
                 assert os.path.isfile(file_path), file_path
                 assert file_path.endswith('.jpg'), file_path
 
-                box_xyxy, plate_name = parse_ccpd(os.path.basename(file_path))
-
-                src_img = cv2.imread(file_path)
-                x1, y1, x2, y2 = box_xyxy
-                plate_img = src_img[y1:y2, x1:x2]
-
-                dst_file_path = os.path.join(dst_data_root, plate_name + f'.jpg')
-                if os.path.exists(dst_file_path):
-                    file_len = len(list(glob.glob(os.path.join(dst_data_root, f"{plate_name}*.jpg"))))
-                    dst_file_path = os.path.join(dst_data_root, plate_name + f'-{file_len}.jpg')
-                # assert not os.path.exists(dst_file_path), f"{file_path}\n{dst_file_path}"
-                cv2.imwrite(dst_file_path, plate_img)
+                save_to_dst(file_path, os.path.basename(file_path), dst_data_root)
 
 
 def process_ccpd2020(data_root, dst_root):
@@ -93,10 +107,7 @@ def process_ccpd2020(data_root, dst_root):
         print('*' * 100)
         print(f"Getting {data_dir} data...")
 
-        if 'test' not in name:
-            dst_data_root = os.path.join(dst_root, "trainval")
-        else:
-            dst_data_root = os.path.join(dst_root, "test")
+        dst_data_root = os.path.join(dst_root, name)
         if not os.path.exists(dst_data_root):
             os.makedirs(dst_data_root)
         print(f"Save to {dst_data_root}")
@@ -106,33 +117,22 @@ def process_ccpd2020(data_root, dst_root):
             assert os.path.isfile(file_path), file_path
             assert file_path.endswith('.jpg'), file_path
 
-            box_xyxy, plate_name = parse_ccpd(file_name)
-
-            src_img = cv2.imread(file_path)
-            x1, y1, x2, y2 = box_xyxy
-            plate_img = src_img[y1:y2, x1:x2]
-
-            dst_file_path = os.path.join(dst_data_root, plate_name + f'.jpg')
-            if os.path.exists(dst_file_path):
-                file_len = len(list(glob.glob(os.path.join(dst_data_root, f"{plate_name}*.jpg"))))
-                dst_file_path = os.path.join(dst_data_root, plate_name + f'-{file_len}.jpg')
-            # assert not os.path.exists(dst_file_path), f"{file_name}\n{dst_file_path}"
-            cv2.imwrite(dst_file_path, plate_img)
+            save_to_dst(file_path, file_name, dst_data_root)
 
 
 def main():
+    dst_root = "../datasets/chinese_license_plate/recog"
     data_root = "../datasets/ccpd"
-    dst_root = "../datasets/ccpd/plate"
 
     ccpd2019_root = os.path.join(data_root, "CCPD2019")
     if os.path.isdir(ccpd2019_root):
-        print(f"Prlocess {ccpd2019_root}")
-        process_ccpd2019(ccpd2019_root, dst_root)
+        print(f"Process {ccpd2019_root}")
+        process_ccpd2019(ccpd2019_root, os.path.join(dst_root, "CCPD2019"))
 
     ccpd2020_root = os.path.join(data_root, "CCPD2020", "ccpd_green")
     if os.path.isdir(ccpd2020_root):
         print(f"Process {ccpd2020_root}")
-        process_ccpd2020(ccpd2020_root, dst_root)
+        process_ccpd2020(ccpd2020_root, os.path.join(dst_root, "CCPD2020"))
 
 
 if __name__ == '__main__':
