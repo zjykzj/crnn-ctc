5846cf66507e01376cbefdbecba1b51f96e4d078
zjykzj <wy163zhuj@163.com>
2024-08-03 15:48:34 +0800

    perf(crnn.py): add crnn_tiny

--- utils/model/crnn.py
+++ utils/model/crnn.py
@@ -14,44 +14,76 @@ import torch.nn.functional as F
 
 class CRNN(nn.Module):
 
-    def __init__(self, in_channel, num_classes, cnn_output_height, use_gru=False):
+    def __init__(self, in_channel, num_classes, cnn_input_height, is_tiny=True, use_gru=True):
         super().__init__()
 
-        # 特征提取层
-        self.cnn = nn.Sequential(
-            nn.Conv2d(in_channel, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1)),
+        if is_tiny:
+            # 特征提取层
+            self.cnn = nn.Sequential(
+                nn.Conv2d(in_channel, 32, kernel_size=3, stride=2, padding=1),
+                nn.BatchNorm2d(32),
+                nn.ReLU(inplace=True),
 
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1)),
+                nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(32),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(32, 32, kernel_size=3, stride=(2, 1), padding=1),
+                nn.BatchNorm2d(32),
+                nn.ReLU(inplace=True),
 
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1)),
+                nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 64, kernel_size=3, stride=(2, 1), padding=1),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
 
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1)),
+                nn.Conv2d(64, 64, kernel_size=2, stride=1, padding=0),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+            )
 
-            nn.Conv2d(512, 512, kernel_size=2, stride=1, padding=0),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-        )
+            cnn_output_height = cnn_input_height // 2 // 2 // 2 - 1
+            rnn_input_size = 64 * cnn_output_height
+            rnn_hidden_size = rnn_input_size // 2
+        else:
+            # 特征提取层
+            self.cnn = nn.Sequential(
+                nn.Conv2d(in_channel, 64, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=2, stride=2),
 
-        rnn_input_size = 512 * cnn_output_height
-        rnn_hidden_size = rnn_input_size // 2
+                nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(128),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+
+                nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1)),
+
+                nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(512),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(512),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1)),
+
+                nn.Conv2d(512, 512, kernel_size=2, stride=1, padding=0),
+                nn.BatchNorm2d(512),
+                nn.ReLU(inplace=True),
+            )
+
+            cnn_output_height = cnn_input_height // 2 // 2 // 2 // 2 - 1
+            rnn_input_size = 512 * cnn_output_height
+            rnn_hidden_size = rnn_input_size // 2
+
         if use_gru:
             self.rnn = nn.GRU(input_size=rnn_input_size, hidden_size=rnn_hidden_size, num_layers=2, batch_first=True,
                               bidirectional=True)
@@ -81,27 +113,33 @@ class CRNN(nn.Module):
         return out
 
 
+def test_model(data, model):
+    print(data.shape)
+
+    t_start = time.time()
+    output = model(data)
+    t_end = time.time()
+    print(f"time: {t_end - t_start}")
+    print(data.shape, output.shape)
+
+
 if __name__ == '__main__':
     import time
+    import copy
 
     # EMNIST
-    model = CRNN(in_channel=1, num_classes=11, cnn_output_height=1)
-
     data = torch.randn(10, 1, 32, 32 * 5)
-    t_start = time.time()
-    output = model(data)
-    t_end = time.time()
-    print(f"time: {t_end - t_start}")
-    print(data.shape, output.shape)
 
+    model = CRNN(in_channel=1, num_classes=11, cnn_input_height=32, is_tiny=False, use_gru=True)
+    test_model(copy.deepcopy(data), model)
+
+    model = CRNN(in_channel=1, num_classes=11, cnn_input_height=32, is_tiny=True, use_gru=True)
+    test_model(copy.deepcopy(data), model)
     # Plate
-    model = CRNN(in_channel=3, num_classes=100, cnn_output_height=2, use_gru=True)
-    print(model)
-
     data = torch.randn(10, 3, 48, 168)
 
-    t_start = time.time()
-    output = model(data)
-    t_end = time.time()
-    print(f"time: {t_end - t_start}")
-    print(data.shape, output.shape)
+    model = CRNN(in_channel=3, num_classes=100, cnn_input_height=48, is_tiny=False, use_gru=True)
+    test_model(copy.deepcopy(data), model)
+
+    model = CRNN(in_channel=3, num_classes=100, cnn_input_height=48, is_tiny=True, use_gru=True)
+    test_model(copy.deepcopy(data), model)
