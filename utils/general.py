17a7363b31633be310bf07c5eaaeb3508df25d7d
zjykzj <wy163zhuj@163.com>
2024-08-13 19:41:30 +0800

    perf(utils/general.py): add model_info and load_crnn

--- utils/general.py
+++ utils/general.py
@@ -8,6 +8,7 @@
 """
 
 import os
+import thop
 import torch
 import random
 
@@ -15,8 +16,10 @@ import platform
 import pkg_resources as pkg
 
 import numpy as np
+from copy import deepcopy
 
 from .logger import LOGGER
+from .model.crnn import CRNN
 
 
 def emojis(str=''):
@@ -49,3 +52,52 @@ def init_seeds(seed=0, deterministic=False):
         torch.backends.cudnn.deterministic = True
         os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
         os.environ['PYTHONHASHSEED'] = str(seed)
+
+
+def model_info(model, model_name, verbose=False, img_shape=(1, 3, 48, 168)):
+    # Model information. img_size may be int or list, i.e. img_size=640 or img_size=[640, 320]
+    n_p = sum(x.numel() for x in model.parameters())  # number parameters
+    n_g = sum(x.numel() for x in model.parameters() if x.requires_grad)  # number gradients
+    if verbose:
+        print(f"{'layer':>5} {'name':>40} {'gradient':>9} {'parameters':>12} {'shape':>20} {'mu':>10} {'sigma':>10}")
+        for i, (name, p) in enumerate(model.named_parameters()):
+            name = name.replace('module_list.', '')
+            print('%5g %40s %9s %12g %20s %10.3g %10.3g' %
+                  (i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()))
+
+    try:  # FLOPs
+        p = next(model.parameters())
+        im = torch.empty(img_shape, device=p.device)  # input image in BCHW format
+        flops = thop.profile(deepcopy(model), inputs=(im,), verbose=False)[0] / 1E9 * 2  # stride GFLOPs
+        fs = f', {flops:.1f} GFLOPs'  # 640x640 GFLOPs
+    except Exception:
+        fs = ''
+
+    print(f"{model_name} summary: {len(list(model.modules()))} layers, {n_p} parameters, {n_g} gradients{fs}")
+
+
+def load_crnn(pretrained=None, device=None, shape=(1, 3, 48, 168), num_classes=100, not_tiny=False, use_lstm=False):
+    model = CRNN(in_channel=shape[1], num_classes=num_classes, cnn_input_height=shape[2], is_tiny=not not_tiny,
+                 use_gru=not use_lstm)
+    if pretrained is not None:
+        if isinstance(pretrained, list):
+            pretrained = pretrained[0]
+        print(f"Loading CRNN pretrained: {pretrained}")
+        ckpt = torch.load(pretrained, map_location='cpu')
+        ckpt = {k.replace("module.", ""): v for k, v in ckpt.items()}
+        model.load_state_dict(ckpt, strict=True)
+    model.eval()
+
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+
+    # Warm
+    for _ in range(3):
+        data = torch.randn(shape).to(device)
+        _ = model(data)
+
+    model_name = os.path.splitext(os.path.basename(pretrained))[0]
+    model_info(model, model_name, verbose=False, img_shape=shape)
+
+    return model, device
